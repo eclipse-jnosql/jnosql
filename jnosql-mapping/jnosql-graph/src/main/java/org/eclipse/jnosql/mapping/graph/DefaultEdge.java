6c63955056482fc1cad26a44144625a81c417b69
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-05 23:37:51 +0000

    feat: define default edge record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-graph/src/main/java/org/eclipse/jnosql/mapping/graph/DefaultEdge.java
+++ jnosql-mapping/jnosql-graph/src/main/java/org/eclipse/jnosql/mapping/graph/DefaultEdge.java
@@ -1,13 +1,48 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
 package org.eclipse.jnosql.mapping.graph;
 
+import org.eclipse.jnosql.communication.Value;
+
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
-record DefaultEdge<S, T>( Object key, S source, T target, String label, Map<String, Object> properties) implements Edge<S, T> {
+record DefaultEdge<S, T>(S source, T target, String label, Map<String, Object> properties) implements Edge<S, T> {
 
 
     @Override
-    public <V> Optional<V> property(String key, Class<V> type) {
+    public Optional<Object> id() {
         return Optional.empty();
+    }
+
+    @Override
+    public <K> Optional<K> id(Class<K> type) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Map<String, Object> properties() {
+        return Collections.unmodifiableMap(properties);
+    }
+
+    @Override
+    public <V> Optional<V> property(String key, Class<V> type) {
+        Objects.requireNonNull(key, "key is required");
+        Objects.requireNonNull(type, "type is required");
+        return Optional.ofNullable(properties.get(key)).map(Value::of).map(v -> v.get(type));
     }
 }
