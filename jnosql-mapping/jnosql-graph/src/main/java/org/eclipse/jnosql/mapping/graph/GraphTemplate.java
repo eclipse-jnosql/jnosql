92958f9923f957788851fb133f3895c16d7c752d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-05 21:49:58 +0000

    feat: generate edge GraphTemplate
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GraphTemplate.java
+++ jnosql-mapping/jnosql-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GraphTemplate.java
@@ -20,6 +20,8 @@ package org.eclipse.jnosql.mapping.graph;
 
 import org.eclipse.jnosql.mapping.semistructured.SemiStructuredTemplate;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 
@@ -64,5 +66,65 @@ public interface GraphTemplate extends SemiStructuredTemplate {
      * @throws NullPointerException if any of the parameters are null
      */
     <T, E> Edge<T, E> edge(T source, Supplier<String> label, E target);
+
+    /**
+     * Creates an edge (relationship) between two entities with additional properties.
+     *
+     * @param <T>        the source entity type
+     * @param <E>        the target entity type
+     * @param source     the source entity (starting vertex)
+     * @param label      the relationship label
+     * @param target     the target entity (ending vertex)
+     * @param properties a map of properties to be associated with the edge
+     * @return the created edge instance with assigned properties
+     * @throws NullPointerException if any of the parameters are null
+     */
+    <T, E> Edge<T, E> edge(T source, String label, E target, Map<String, Object> properties);
+
+    /**
+     * Creates an edge (relationship) between two entities with additional properties and a dynamic label.
+     *
+     * @param <T>        the source entity type
+     * @param <E>        the target entity type
+     * @param source     the source entity (starting vertex)
+     * @param label      a supplier that dynamically generates the relationship label
+     * @param target     the target entity (ending vertex)
+     * @param properties a map of properties to be associated with the edge
+     * @return the created edge instance with assigned properties
+     * @throws NullPointerException if any of the parameters are null
+     */
+    <T, E> Edge<T, E> edge(T source, Supplier<String> label, E target, Map<String, Object> properties);
+
+    /**
+     * Saves an edge (relationship) into the graph database.
+     *
+     * @param <T>  the source entity type
+     * @param <E>  the target entity type
+     * @param edge the edge entity representing the relationship
+     * @return the saved edge instance with assigned properties or identifiers
+     * @throws NullPointerException if the edge is null
+     */
+    <T, E> Edge<T, E> edge(Edge<T, E> edge);
+
+    /**
+     * Deletes an edge (relationship) from the graph database by its unique identifier.
+     *
+     * @param <K> the type of the edge identifier
+     * @param id  the unique identifier of the edge to delete
+     * @throws NullPointerException if the ID is null
+     */
+    <K> void deleteEdge(K id);
+
+    /**
+     * Finds an edge (relationship) in the graph database by its unique identifier.
+     *
+     * @param <K> the type of the edge identifier
+     * @param <T> the source entity type
+     * @param <E> the target entity type
+     * @param id  the unique identifier of the edge
+     * @return an {@link Optional} containing the edge if found, otherwise an empty {@link Optional}
+     * @throws NullPointerException if the ID is null
+     */
+    <K, T, E> Optional<Edge<T, E>> findEdgeById(K id);
 
 }
