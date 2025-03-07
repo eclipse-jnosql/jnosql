0e66110d28a5549f6bbc6b7e989e6ddc17daa4b4
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-07 08:10:06 +0000

    feat: reduce code on beans
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/query/CustomRepositoryDocumentBean.java
+++ jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/query/CustomRepositoryDocumentBean.java
@@ -14,23 +14,12 @@
  */
 package org.eclipse.jnosql.mapping.document.query;
 
-import jakarta.enterprise.context.spi.CreationalContext;
 import org.eclipse.jnosql.mapping.DatabaseQualifier;
 import org.eclipse.jnosql.mapping.DatabaseType;
-import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.core.spi.AbstractBean;
-import org.eclipse.jnosql.mapping.core.util.AnnotationLiteralUtil;
 import org.eclipse.jnosql.mapping.document.DocumentTemplate;
-import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.semistructured.SemiStructuredTemplate;
 import org.eclipse.jnosql.mapping.semistructured.query.CustomRepositoryBean;
-import org.eclipse.jnosql.mapping.semistructured.query.CustomRepositoryHandler;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Proxy;
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 
 /**
@@ -51,7 +40,7 @@ public class CustomRepositoryDocumentBean<T> extends CustomRepositoryBean<T> {
     }
 
     @Override
-    protected Class<?> getTemplateClass() {
+    protected Class<? extends SemiStructuredTemplate> getTemplateClass() {
         return DocumentTemplate.class;
     }
 
