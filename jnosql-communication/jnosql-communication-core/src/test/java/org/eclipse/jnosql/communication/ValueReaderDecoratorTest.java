20d45524b4dd239d30a5d959594834e3899b2939
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-29 04:38:47 +0000

    refac: update value reader decorator
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-core/src/test/java/org/eclipse/jnosql/communication/ValueReaderDecoratorTest.java
+++ jnosql-communication/jnosql-communication-core/src/test/java/org/eclipse/jnosql/communication/ValueReaderDecoratorTest.java
@@ -17,6 +17,7 @@
  */
 package org.eclipse.jnosql.communication;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -26,26 +27,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class ValueReaderDecoratorTest {
 
-    private final ValueReaderDecorator serviceLoader = ValueReaderDecorator.getInstance();
+    private final ValueReaderDecorator reader = ValueReaderDecorator.getInstance();
 
     @Test
     @DisplayName("Should be able to convert")
     void shouldConvert() {
-        Number convert = serviceLoader.read(Number.class, "10D");
+        Number convert = reader.read(Number.class, "10D");
         assertThat(convert).isEqualTo(10D);
     }
 
     @Test
     @DisplayName("Should be able to cast")
     void shouldCastObject() {
-        Bean name = serviceLoader.read(Bean.class, new Bean());
+        Bean name = reader.read(Bean.class, new Bean());
         assertThat(name.getClass()).isEqualTo(Bean.class);
     }
 
     @Test
     @DisplayName("Should throw UnsupportedOperationException during read when class is not supported")
     void shouldReturnErrorWhenTypeIsNotSupported() {
-        assertThatThrownBy(() -> serviceLoader.read(Bean.class, "name"))
+        assertThatThrownBy(() -> reader.read(Bean.class, "name"))
                 .isInstanceOf(UnsupportedOperationException.class)
                 .hasMessage("The type class org.eclipse.jnosql.communication.ValueReaderDecoratorTest$Bean is not supported yet");
     }
@@ -53,14 +54,26 @@ class ValueReaderDecoratorTest {
     @Test
     @DisplayName("Should check for compatibility")
     void shouldReturnIfIsCompatible() {
-        assertThat(serviceLoader.test(Integer.class)).isTrue();
+        assertThat(reader.test(Integer.class)).isTrue();
     }
 
     @Test
     @DisplayName("Should check for incompatibility")
     void shouldReturnIfIsNotCompatible() {
-        assertThat(serviceLoader.test(Bean.class)).isFalse();
-        assertFalse(serviceLoader.test(Bean.class));
+        assertThat(reader.test(Bean.class)).isFalse();
+        assertFalse(reader.test(Bean.class));
+    }
+
+    @Test
+    @DisplayName("Should check if the reader is compatible with the type")
+    void shouldConvertArray() {
+        Bean[] read = reader.read(Bean[].class, new Bean());
+        SoftAssertions.assertSoftly(soft->{;
+            soft.assertThat(read).isNotNull();
+            soft.assertThat(read.length).isEqualTo(1);
+            soft.assertThat(read[0]).isNotNull();
+            soft.assertThat(read[0].getClass()).isEqualTo(Bean.class);
+        });
     }
 
     static class Bean {
