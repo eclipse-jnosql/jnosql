504443732cf25de4ab55f6a2ff537ee549f3b476
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-29 03:32:50 +0000

    feat: update the code style
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-core/src/main/java/org/eclipse/jnosql/communication/reader/ArrayReader.java
+++ jnosql-communication/jnosql-communication-core/src/main/java/org/eclipse/jnosql/communication/reader/ArrayReader.java
@@ -22,7 +22,6 @@ import org.eclipse.jnosql.communication.ValueReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * The {@code ArrayReader} is a {@link ValueReader} that reads an array of
