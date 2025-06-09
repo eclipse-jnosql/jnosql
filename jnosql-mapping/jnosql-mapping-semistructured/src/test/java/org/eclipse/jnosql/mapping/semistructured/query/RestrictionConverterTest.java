eeeb57a230189173f1eeeded7c18ba33389034b7
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-09 09:40:46 +0100

    test: update restriction converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/RestrictionConverterTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/RestrictionConverterTest.java
@@ -14,8 +14,62 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.query;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.data.restrict.Restriction;
+import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
+import org.eclipse.jnosql.communication.Condition;
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.reflection.Reflections;
+import org.eclipse.jnosql.mapping.reflection.spi.ReflectionEntityMetadataExtension;
+import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
+import org.eclipse.jnosql.mapping.semistructured.MockProducer;
+import org.eclipse.jnosql.mapping.semistructured.entities.Product;
+import org.eclipse.jnosql.mapping.semistructured.entities._Product;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+
+@EnableAutoWeld
+@AddPackages(value = {Converters.class, EntityConverter.class})
+@AddPackages(MockProducer.class)
+@AddPackages(Reflections.class)
+@AddExtensions({ReflectionEntityMetadataExtension.class})
 class RestrictionConverterTest {
+
+    @Inject
+    private EntitiesMetadata entities;
+
+    private EntityMetadata entityMetadata;
+
+    @BeforeEach
+    void setUp() {
+        entityMetadata = entities.get(Product.class);
+    }
+
+
+    @Test
+    void shouldExecuteEqualsCondition() {
+        Restriction<Product> equalTo = _Product.name.equalTo("Macbook Pro");
+
+        Optional<CriteriaCondition> optional = RestrictionConverter.INSTANCE.parser(equalTo, entityMetadata);
+
+        SoftAssertions.assertSoftly(soft ->{
+            soft.assertThat(optional).isPresent();
+            var condition = optional.orElseThrow();
+            var element = condition.element();
+
+            soft.assertThat(condition.condition()).isEqualTo(Condition.EQUALS);
+            soft.assertThat(element.name()).isEqualTo(_Product.NAME);
+            soft.assertThat(element.get()).isEqualTo("Macbook Pro");
+        });
+    }
 
 }
