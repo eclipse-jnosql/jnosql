5aaff96783dba3a7616baf97708278a4984e131d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-22 08:50:26 +0100

    feat: Projector converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/ProjectionConverter.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/ProjectionConverter.java
@@ -1,4 +1,44 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *   Maximillian Arruda
+ */
 package org.eclipse.jnosql.mapping.reflection;
 
-public class ProjectionConverter {
+import org.eclipse.jnosql.mapping.metadata.ProjectionMetadata;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+class ProjectionConverter implements Function<Class<?>, ProjectionMetadata> {
+
+    private static final Logger LOGGER = Logger.getLogger(ProjectionConverter.class.getName());
+
+    private final Reflections reflections;
+
+    ProjectionConverter() {
+        this.reflections = new Reflections();
+    }
+
+    @Override
+    public ProjectionMetadata apply(Class<?> type) {
+        Objects.requireNonNull(type, "type is required");
+        if(!type.isRecord()) {
+            throw new IllegalArgumentException("The type " + type.getName() + " is not record");
+        }
+        LOGGER.fine(() -> "Converting " + type.getName() + " to ProjectionMetadata");
+
+        return null;
+    }
 }
