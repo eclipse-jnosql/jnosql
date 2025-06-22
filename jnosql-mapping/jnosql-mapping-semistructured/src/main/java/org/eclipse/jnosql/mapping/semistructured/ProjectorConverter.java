125fc95750ed8da083cec66de68acd046fc337ff
Otavio Santana <otaviopolianasantana@gmail.com>
2025-06-22 17:09:11 +0100

    feat: update projecttor converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/ProjectorConverter.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/ProjectorConverter.java
@@ -14,5 +14,33 @@
  */
 package org.eclipse.jnosql.mapping.semistructured;
 
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.jnosql.mapping.metadata.ProjectionMetadata;
+
+import java.util.Objects;
+
+/**
+ * A converter that transforms an entity into a projection based on the provided metadata. This class is designed to be
+ * used in a Jakarta EE environment, specifically within an application scope.
+ **/
+@ApplicationScoped
 public class ProjectorConverter {
+
+
+    /**
+     * Converts the given entity to a projection based on the provided metadata.
+     *
+     * @param entity the entity to be converted, must not be null
+     * @param metadata the metadata defining the projection, must not be null
+     * @param <T> the type of the entity
+     * @param <P> the type of the projection
+     * @return a projection of type P based on the entity and metadata
+     * @throws NullPointerException if either entity or metadata is null
+     */
+    public <T, P> P map(T entity, ProjectionMetadata metadata) {
+        Objects.requireNonNull(entity, "entity is required");
+        Objects.requireNonNull(metadata, "metadata is required");
+    }
+
 }
