012030a0103b2ce3c17cbbac235dba2498f3aeac
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-30 19:09:08 +0100

    feat: update parser with cached version on updatequery
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/UpdateQueryParser.java
+++ jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/UpdateQueryParser.java
@@ -14,7 +14,7 @@ package org.eclipse.jnosql.communication.semistructured;
 import org.eclipse.jnosql.communication.Params;
 import org.eclipse.jnosql.communication.QueryException;
 import org.eclipse.jnosql.communication.query.UpdateItem;
-import org.eclipse.jnosql.communication.query.data.UpdateParser;
+import org.eclipse.jnosql.communication.query.data.UpdateProvider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,8 +60,7 @@ public final class UpdateQueryParser implements BiFunction<org.eclipse.jnosql.co
     }
 
     private UpdateQuery getQuery(String query, Params params, CommunicationObserverParser observer) {
-        var converter = new UpdateParser();
-        var updateQuery = converter.apply(query);
+        var updateQuery = UpdateProvider.INSTANCE.apply(query);
         return getQuery(params, observer, updateQuery);
     }
 
@@ -82,8 +81,7 @@ public final class UpdateQueryParser implements BiFunction<org.eclipse.jnosql.co
 
     private UpdateQuery getQuery(String query, CommunicationObserverParser observer) {
 
-        var converter = new UpdateParser();
-        var updateQuery = converter.apply(query);
+        var updateQuery = UpdateProvider.INSTANCE.apply(query);
 
         var entity = observer.fireEntity(updateQuery.entity());
         Params params = Params.newParams();
