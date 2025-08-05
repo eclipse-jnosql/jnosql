dff486e435b0facc667a733e9d9cacd9e67d2d2d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-05 08:16:23 +0100

    test: include configuration test on scenario
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-core/src/test/java/org/eclipse/jnosql/communication/ConfigurationsTest.java
+++ jnosql-communication/jnosql-communication-core/src/test/java/org/eclipse/jnosql/communication/ConfigurationsTest.java
@@ -16,8 +16,33 @@
  */
 package org.eclipse.jnosql.communication;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.Map;
 
 class ConfigurationsTest {
 
+    private static final Map<Configurations, String> EXPECTED_VALUES = Map.of(
+            Configurations.USER, "jakarta.nosql.user",
+            Configurations.PASSWORD, "jakarta.nosql.password",
+            Configurations.HOST, "jakarta.nosql.host",
+            Configurations.ENCRYPTION, "jakarta.nosql.settings.encryption",
+            Configurations.CURSOR_PAGINATION_MULTIPLE_SORTING, "org.eclipse.jnosql.pagination.cursor"
+    );
+
+    @ParameterizedTest
+    @EnumSource(Configurations.class)
+    void shouldReturnExpectedConfigurationValue(Configurations config) {
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(config.get())
+                    .as("Check get() for " + config.name())
+                    .isEqualTo(EXPECTED_VALUES.get(config));
+
+            soft.assertThat(config)
+                    .as("Ensure mapping is defined for " + config.name())
+                    .isIn(EXPECTED_VALUES.keySet());
+        });
+    }
 }
