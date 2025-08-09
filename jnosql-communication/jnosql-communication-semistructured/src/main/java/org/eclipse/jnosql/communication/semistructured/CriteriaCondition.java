4a96ba94661299839c8b04d75abea983a6757ff5
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-09 11:21:04 +0100

    feat: update criteria condition
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/CriteriaCondition.java
+++ jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/CriteriaCondition.java
@@ -502,7 +502,7 @@ public final class CriteriaCondition {
      * scan a semistructured NoSQL database with the same name and the value matches the underlying condition
      * ignoring the case.
      *
-     * @param element an element instance
+     * @param condition an element instance
      * @return a {@link CriteriaCondition} with {@link Condition#IGNORE_CASE}
      * @throws NullPointerException when the element is null
      */
