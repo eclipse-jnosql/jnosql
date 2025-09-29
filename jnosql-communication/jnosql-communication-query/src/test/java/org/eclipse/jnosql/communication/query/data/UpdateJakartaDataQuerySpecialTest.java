4a27f2113f0713b5d27cebbc31a862dbda375ca8
Otavio Santana <otaviopolianasantana@gmail.com>
2025-09-29 20:25:48 +0100

    test: add parameterized tests for UPDATE queries with boolean conditions

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/UpdateJakartaDataQuerySpecialTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/UpdateJakartaDataQuerySpecialTest.java
@@ -17,6 +17,8 @@ import org.eclipse.jnosql.communication.query.BooleanQueryValue;
 import org.eclipse.jnosql.communication.query.NullQueryValue;
 import org.eclipse.jnosql.communication.query.QueryCondition;
 import org.eclipse.jnosql.communication.query.QueryValue;
+import org.eclipse.jnosql.communication.query.StringQueryValue;
+import org.eclipse.jnosql.communication.query.UpdateItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,83 +26,40 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
 class UpdateJakartaDataQuerySpecialTest {
-    private SelectParser selectParser;
+    private UpdateParser updateParser;
 
     @BeforeEach
     void setUp() {
-        selectParser = new SelectParser();
+        updateParser = new UpdateParser();
     }
 
     @ParameterizedTest(name = "Should parser the query {0}")
-    @ValueSource(strings = {"FROM entity WHERE active = true"})
+    @ValueSource(strings = {"UPDATE entity SET active = true"})
     void shouldValidateTrue(String query){
-        var selectQuery = selectParser.apply(query, null);
+        var selectQuery = updateParser.apply(query);
 
         SoftAssertions.assertSoftly(soft -> {
-            soft.assertThat(selectQuery.fields()).isEmpty();
             soft.assertThat(selectQuery.entity()).isEqualTo("entity");
-            soft.assertThat(selectQuery.where()).isNotEmpty();
-            var where = selectQuery.where().orElseThrow();
-            var condition = where.condition();
-            soft.assertThat(condition.condition()).isEqualTo(Condition.EQUALS);
-            soft.assertThat(condition.name()).isEqualTo("active");
-            soft.assertThat(condition.value()).isEqualTo(BooleanQueryValue.TRUE);
+            List<UpdateItem> items = selectQuery.set();
+            soft.assertThat(items).hasSize(1);
+
+            soft.assertThat(items).isNotNull().hasSize(1)
+                    .contains(JDQLUpdateItem.of("active", BooleanQueryValue.TRUE));
         });
     }
 
     @ParameterizedTest(name = "Should parser the query {0}")
-    @ValueSource(strings = {"FROM entity WHERE active = false"})
+    @ValueSource(strings = {"UPDATE entity SET active = false"})
     void shouldValidateFalse(String query){
-        var selectQuery = selectParser.apply(query, null);
+        var selectQuery = updateParser.apply(query);
 
         SoftAssertions.assertSoftly(soft -> {
-            soft.assertThat(selectQuery.fields()).isEmpty();
             soft.assertThat(selectQuery.entity()).isEqualTo("entity");
-            soft.assertThat(selectQuery.where()).isNotEmpty();
-            var where = selectQuery.where().orElseThrow();
-            var condition = where.condition();
-            soft.assertThat(condition.condition()).isEqualTo(Condition.EQUALS);
-            soft.assertThat(condition.name()).isEqualTo("active");
-            soft.assertThat(condition.value()).isEqualTo(BooleanQueryValue.FALSE);
-        });
-    }
+            List<UpdateItem> items = selectQuery.set();
+            soft.assertThat(items).hasSize(1);
 
-    @ParameterizedTest(name = "Should parser the query {0}")
-    @ValueSource(strings = {"FROM entity WHERE license IS NULL"})
-    void shouldCheckIsNull(String query){
-        var selectQuery = selectParser.apply(query, null);
-
-        SoftAssertions.assertSoftly(soft -> {
-            soft.assertThat(selectQuery.fields()).isEmpty();
-            soft.assertThat(selectQuery.entity()).isEqualTo("entity");
-            soft.assertThat(selectQuery.where()).isNotEmpty();
-            var where = selectQuery.where().orElseThrow();
-            var condition = where.condition();
-            soft.assertThat(condition.condition()).isEqualTo(Condition.EQUALS);
-            soft.assertThat(condition.name()).isEqualTo("license");
-            soft.assertThat(condition.value()).isEqualTo(NullQueryValue.INSTANCE);
-        });
-    }
-
-    @SuppressWarnings("unchecked")
-    @ParameterizedTest(name = "Should parser the query {0}")
-    @ValueSource(strings = {"FROM entity WHERE license IS NOT NULL"})
-    void shouldCheckIsNotNull(String query){
-        var selectQuery = selectParser.apply(query, null);
-
-        SoftAssertions.assertSoftly(soft -> {
-            soft.assertThat(selectQuery.fields()).isEmpty();
-            soft.assertThat(selectQuery.entity()).isEqualTo("entity");
-            soft.assertThat(selectQuery.where()).isNotEmpty();
-            var where = selectQuery.where().orElseThrow();
-            var condition = where.condition();
-            soft.assertThat(condition.condition()).isEqualTo(Condition.NOT);
-            QueryValue<?> negation = condition.value();
-            List<QueryCondition> value = (List<QueryCondition>) negation.get();
-            QueryCondition queryCondition = value.getFirst();
-            soft.assertThat(queryCondition.condition()).isEqualTo(Condition.EQUALS);
-            soft.assertThat(queryCondition.name()).isEqualTo("license");
-            soft.assertThat(queryCondition.value()).isEqualTo(NullQueryValue.INSTANCE);
+            soft.assertThat(items).isNotNull().hasSize(1)
+                    .contains(JDQLUpdateItem.of("active", BooleanQueryValue.FALSE));
         });
     }
 
