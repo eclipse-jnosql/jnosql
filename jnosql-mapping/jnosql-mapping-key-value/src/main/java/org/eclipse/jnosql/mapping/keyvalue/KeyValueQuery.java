133776bf5b19baaa1175f1ab23496538cf1010e0
Otavio Santana <otaviopolianasantana@gmail.com>
2025-10-24 22:53:37 +0100

    test: add parameterized test for unsupported queries with non-ID attributes

--- jnosql-mapping/jnosql-mapping-key-value/src/main/java/org/eclipse/jnosql/mapping/keyvalue/KeyValueQuery.java
+++ jnosql-mapping/jnosql-mapping-key-value/src/main/java/org/eclipse/jnosql/mapping/keyvalue/KeyValueQuery.java
@@ -15,12 +15,19 @@
 package org.eclipse.jnosql.mapping.keyvalue;
 
 
+import jakarta.nosql.MappingException;
 import jakarta.nosql.Query;
+import org.eclipse.jnosql.communication.Condition;
 import org.eclipse.jnosql.communication.query.DeleteQuery;
+import org.eclipse.jnosql.communication.query.QueryCondition;
 import org.eclipse.jnosql.communication.query.SelectQuery;
+import org.eclipse.jnosql.communication.query.Where;
 import org.eclipse.jnosql.communication.query.data.DeleteProvider;
 import org.eclipse.jnosql.communication.query.data.QueryType;
 import org.eclipse.jnosql.communication.query.data.SelectProvider;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,6 +49,8 @@ final class KeyValueQuery implements Query {
 
     private final String param;
 
+    private final FieldMetadata id;
+
 
     private KeyValueQuery(String query,
                           AbstractKeyValueTemplate template,
@@ -49,7 +58,8 @@ final class KeyValueQuery implements Query {
                           SelectQuery selectQuery,
                           DeleteQuery deleteQuery,
                           Object value,
-                          String param) {
+                          String param,
+                          FieldMetadata id) {
         this.query = query;
         this.template = template;
         this.type = type;
@@ -57,6 +67,7 @@ final class KeyValueQuery implements Query {
         this.deleteQuery = deleteQuery;
         this.value = value;
         this.param = param;
+        this.id = id;
     }
 
     static KeyValueQuery of(String query, AbstractKeyValueTemplate template, QueryType type) {
@@ -64,12 +75,29 @@ final class KeyValueQuery implements Query {
         DeleteQuery deleteQuery = null;
         Object value = null;
         String param = null;
+        EntitiesMetadata entities = template.getConverter().getEntities();
+        FieldMetadata id = null;
+
         if(QueryType.SELECT.equals(type)) {
             selectQuery = selectQuery(query);
+            var entityName = selectQuery.entity();
+            var metadata = entities.findByName(entityName);
+            var idAttribute = metadata.id().orElseThrow(() -> new MappingException("The entity does not have an attribute with jakarta.nosql.Id annotation"));
+            Where where = selectQuery.where().orElseThrow();
+            var condition = where.condition();
+            if (!(Condition.EQUALS.equals(condition.condition())
+                    || Condition.IN.equals(condition.condition()))) {
+                throw new UnsupportedOperationException("the condition must be equals or in on key-value databases, " +
+                        "the query is: " + query);
+            }
+            String name = condition.name();
+            if(!idAttribute.name().equals(name)) {
+                throw new UnsupportedOperationException("The key-value Mapper query only support the id attribute: " + idAttribute.name() + " at the entity: " + entityName);
+            }
         } else {
             deleteQuery = DeleteProvider.INSTANCE.apply(query);
         }
-        return new KeyValueQuery(query, template, type, selectQuery, deleteQuery, value, param);
+        return new KeyValueQuery(query, template, type, selectQuery, deleteQuery, value, param, id);
     }
 
     private static SelectQuery selectQuery(String query) {
