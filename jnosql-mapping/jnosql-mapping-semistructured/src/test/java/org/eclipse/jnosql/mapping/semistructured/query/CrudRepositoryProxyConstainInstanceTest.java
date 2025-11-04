77cbb0bf90d40132740b78e1d0cde17e06d0d502
Otavio Santana <otaviopolianasantana@gmail.com>
2025-11-04 18:55:11 +0000

    test: add between and notBetween condition tests in CrudRepositoryProxyConstainInstanceTest

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/CrudRepositoryProxyConstainInstanceTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/CrudRepositoryProxyConstainInstanceTest.java
@@ -55,6 +55,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.eclipse.jnosql.communication.Condition.BETWEEN;
 import static org.eclipse.jnosql.communication.Condition.EQUALS;
 import static org.eclipse.jnosql.communication.Condition.GREATER_THAN;
 import static org.eclipse.jnosql.communication.Condition.IN;
@@ -270,6 +271,52 @@ class CrudRepositoryProxyConstainInstanceTest {
         });
     }
 
+    @Test
+    void shouldBetween() {
+
+        when(template.select(any(SelectQuery.class)))
+                .thenReturn(Stream.of(new Product()));
+
+        repository.between(Between.bounds(BigDecimal.ONE, BigDecimal.TEN));
+        ArgumentCaptor<SelectQuery> captor = ArgumentCaptor.forClass(SelectQuery.class);
+        verify(template).select(captor.capture());
+        SelectQuery query = captor.getValue();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(query.name()).isEqualTo("Product");
+            softly.assertThat(query.condition()).isPresent();
+            CriteriaCondition condition = query.condition().orElseThrow();
+            softly.assertThat(condition).isInstanceOf(CriteriaCondition.class);
+            softly.assertThat(condition.condition()).isEqualTo(BETWEEN);
+            softly.assertThat(condition.element()).isEqualTo(Element.of(_Product.PRICE, List.of(BigDecimal.ONE, BigDecimal.TEN)));
+        });
+    }
+
+    @Test
+    void shouldNotBetween() {
+
+        when(template.select(any(SelectQuery.class)))
+                .thenReturn(Stream.of(new Product()));
+
+        repository.notBetween(NotBetween.bounds(BigDecimal.ONE, BigDecimal.TEN));
+        ArgumentCaptor<SelectQuery> captor = ArgumentCaptor.forClass(SelectQuery.class);
+        verify(template).select(captor.capture());
+        SelectQuery query = captor.getValue();
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(query.name()).isEqualTo("Product");
+            softly.assertThat(query.condition()).isPresent();
+            CriteriaCondition condition = query.condition().orElseThrow();
+            softly.assertThat(condition).isInstanceOf(CriteriaCondition.class);
+            softly.assertThat(condition.condition()).isEqualTo(NOT);
+            var criteriaCondition = condition.element().get(CriteriaCondition.class);
+            softly.assertThat(criteriaCondition.condition()).isEqualTo(BETWEEN);
+            softly.assertThat(criteriaCondition.element()).isEqualTo(Element.of(_Product.PRICE, List.of(BigDecimal.ONE,
+                    BigDecimal.TEN)));
+        });
+    }
+
+
 
     public interface ProductRepository extends CrudRepository<Product, String> {
 
@@ -287,7 +334,7 @@ class CrudRepositoryProxyConstainInstanceTest {
         List<Product> between(@By(_Product.PRICE) Between<BigDecimal> price);
 
         @Find
-        List<Product> botBetween(@By(_Product.PRICE) NotBetween<BigDecimal> price);
+        List<Product> notBetween(@By(_Product.PRICE) NotBetween<BigDecimal> price);
         @Find
         List<Product> greaterThan(@By(_Product.PRICE) GreaterThan<BigDecimal> price);
 
