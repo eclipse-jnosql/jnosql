ff6e5bc92080dc145bd31eabd1d9d804c7f9e8a5
Otavio Santana <otaviopolianasantana@gmail.com>
2025-11-11 08:24:26 +0000

    feat: expand RepositoryType enum with additional repository operation types

--- jnosql-mapping/jnosql-mapping-api-core/src/main/java/org/eclipse/jnosql/mapping/metadata/repository/RepositoryType.java
+++ jnosql-mapping/jnosql-mapping-api-core/src/main/java/org/eclipse/jnosql/mapping/metadata/repository/RepositoryType.java
@@ -14,5 +14,84 @@
  */
 package org.eclipse.jnosql.mapping.metadata.repository;
 
+import jakarta.data.repository.BasicRepository;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Delete;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Insert;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Save;
+import jakarta.data.repository.Update;
+
+/**
+ * It defines the operation that might be from the Method
+ */
 public enum RepositoryType {
+
+    /**
+     * Methods from either {@link CrudRepository}, {@link  BasicRepository} and {@link  org.eclipse.jnosql.mapping.NoSQLRepository}
+     */
+    DEFAULT,
+    /**
+     * General query method returning the repository type.It starts with "findBy" key word
+     */
+    FIND_BY,
+    /**
+     * Delete query method returning either no result (void) or the delete count. It starts with "deleteBy" keyword
+     */
+    DELETE_BY,
+    /**
+     * Method that has the "FindAll" keyword
+     */
+    FIND_ALL,
+    /**
+     * Count projection returning a numeric result. It starts and ends with "countAll" keyword
+     */
+    COUNT_ALL,
+    /**
+     * Count projection returning a numeric result. It starts with "countBy" keyword
+     */
+    COUNT_BY,
+    /**
+     * Exists projection, returning typically a boolean result. It starts with "existsBy" keyword
+     */
+    EXISTS_BY,
+
+    /**
+     * Methods from {@link Object}
+     */
+    OBJECT_METHOD,
+    /**
+     * The method that belongs to the interface using a default method.
+     */
+    DEFAULT_METHOD,
+    /**
+     * The method that belongs to the interface using a custom repository.
+     */
+    CUSTOM_REPOSITORY,
+    /**
+     * Method that has {@link Query} annotation
+     */
+    QUERY,
+    /**
+     * Method that has {@link jakarta.data.repository.Save} annotation
+     */
+    SAVE,
+    /**
+     * The last condition is parameter based. That will match the parameter in a simple query.
+     * It will check by {@link jakarta.data.repository.Find} annotation
+     */
+    PARAMETER_BASED,
+    /**
+     * Method that has {@link jakarta.data.repository.Insert} annotation
+     */
+    INSERT,
+    /**
+     * Method that has {@link jakarta.data.repository.Delete} annotation
+     */
+    DELETE,
+    /**
+     * Method that has {@link jakarta.data.repository.Update} annotation
+     */
+    UPDATE,
 }
