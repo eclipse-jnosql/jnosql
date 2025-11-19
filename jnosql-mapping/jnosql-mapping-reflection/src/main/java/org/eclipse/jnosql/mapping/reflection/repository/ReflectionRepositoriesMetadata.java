576e7ac9c8ed808d71b945b7712ae6be600e9e22
Otavio Santana <otaviopolianasantana@gmail.com>
2025-11-19 19:46:23 +0000

    feat: implement repository metadata loading and logging in ReflectionRepositoriesMetadata

--- jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/repository/ReflectionRepositoriesMetadata.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/repository/ReflectionRepositoriesMetadata.java
@@ -15,19 +15,38 @@
 package org.eclipse.jnosql.mapping.reflection.repository;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.jnosql.mapping.metadata.ClassScanner;
 import org.eclipse.jnosql.mapping.metadata.repository.RepositoriesMetadata;
 import org.eclipse.jnosql.mapping.metadata.repository.RepositoryMetadata;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Logger;
 
 
 @ApplicationScoped
 class ReflectionRepositoriesMetadata implements RepositoriesMetadata {
 
+    private static final Logger LOGGER = Logger.getLogger(ReflectionRepositoriesMetadata.class.getName());
+
+    private Map<Class<?>, RepositoryMetadata> repositories;
+
+    ReflectionRepositoriesMetadata() {
+        Set<Class<?>> repositoriesType = ClassScanner.load().repositories();
+        LOGGER.fine("Found repositories: " + repositoriesType);
+        var supplier = ReflectionRepositorySupplier.INSTANCE;
+        for (Class<?> type : repositoriesType) {
+            RepositoryMetadata metadata = supplier.apply(type);
+            repositories.put(type, metadata);
+        }
+
+    }
+
     @Override
     public Optional<RepositoryMetadata> get(Class<?> type) {
         Objects.requireNonNull(type, "type is required");
-        return Optional.empty();
+        return Optional.ofNullable(repositories.get(type));
     }
 }
