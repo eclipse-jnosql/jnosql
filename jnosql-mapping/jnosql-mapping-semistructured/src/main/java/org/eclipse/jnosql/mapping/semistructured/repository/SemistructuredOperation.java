bdc154879070b236abe7df4e2ae2efad402a53ea
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-07 06:18:08 +0000

    feat: refactor selectQuery method to include inheritance handling in SemistructuredOperation

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredOperation.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredOperation.java
@@ -14,19 +14,25 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.repository;
 
+import jakarta.data.repository.First;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.jnosql.communication.query.method.SelectMethodProvider;
 import org.eclipse.jnosql.communication.semistructured.CommunicationObserverParser;
+import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
+import org.eclipse.jnosql.communication.semistructured.Element;
 import org.eclipse.jnosql.communication.semistructured.SelectQuery;
 import org.eclipse.jnosql.communication.semistructured.SelectQueryParser;
 import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.core.util.ParamsBinder;
 import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.InheritanceMetadata;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+import org.eclipse.jnosql.mapping.semistructured.MappingQuery;
 import org.eclipse.jnosql.mapping.semistructured.query.RepositorySemiStructuredObserverParser;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
@@ -50,16 +56,6 @@ class SemistructuredOperation {
         this.paramsBinderMap = new ConcurrentHashMap<>();
     }
 
-    private CommunicationObserverParser observer(EntityMetadata entityMetadata) {
-        Class<?> entityType = entityMetadata.type();
-        return parsers.computeIfAbsent(entityType,key -> new RepositorySemiStructuredObserverParser(entityMetadata));
-    }
-
-    private ParamsBinder paramsBinder(EntityMetadata entityMetadata) {
-        Class<?> entityType = entityMetadata.type();
-        return paramsBinderMap.computeIfAbsent(entityType,key -> new ParamsBinder(entityMetadata, converters));
-    }
-
 
     public SelectQuery selectQuery(RepositoryInvocationContext context) {
         var method = context.method();
@@ -73,6 +69,34 @@ class SemistructuredOperation {
         var params = queryParams.params();
         var paramsBinder = this.paramsBinder(entityMetadata);
         paramsBinder.bind(params, parameters, method.name());
+        return includeInheritance(query, entityMetadata);
+    }
+
+
+    private CommunicationObserverParser observer(EntityMetadata entityMetadata) {
+        Class<?> entityType = entityMetadata.type();
+        return parsers.computeIfAbsent(entityType,key -> new RepositorySemiStructuredObserverParser(entityMetadata));
+    }
+
+    private ParamsBinder paramsBinder(EntityMetadata entityMetadata) {
+        Class<?> entityType = entityMetadata.type();
+        return paramsBinderMap.computeIfAbsent(entityType,key -> new ParamsBinder(entityMetadata, converters));
+    }
+
+    private SelectQuery includeInheritance(SelectQuery query, EntityMetadata metadata) {
+        if (metadata.inheritance().isPresent()) {
+            InheritanceMetadata inheritanceMetadata = metadata.inheritance().orElseThrow();
+            if (!inheritanceMetadata.parent().equals(metadata.type())) {
+                CriteriaCondition condition = CriteriaCondition.eq(Element.of(inheritanceMetadata.discriminatorColumn(),
+                        inheritanceMetadata.discriminatorValue()));
+                if (query.condition().isPresent()) {
+                    CriteriaCondition columnCondition = query.condition().orElseThrow();
+                    condition = condition.and(columnCondition);
+                }
+                return new MappingQuery(query.sorts(), query.limit(), query.skip(),
+                        condition, query.name(), query.columns());
+            }
+        }
         return query;
     }
 
