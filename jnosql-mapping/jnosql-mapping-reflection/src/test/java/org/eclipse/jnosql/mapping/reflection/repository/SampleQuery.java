c571075bd23fb37d04af419ca8484519e49b8307
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-11 20:27:24 +0000

    feat: add SampleQuery annotation for defining sample queries in repository methods

---
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/repository/SampleQuery.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.reflection.repository;
+
+import org.eclipse.jnosql.mapping.ProviderQuery;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@ProviderQuery("sample")
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SampleQuery {
+
+    String value();
+}
