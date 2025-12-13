9dedc7ef85dca3faf4647a4c55300e64a8d06a24
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-13 07:28:38 +0000

    test: implement execute method in SampleQueryProviderHandler for query execution

--- jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/operations/SampleQueryProviderHandler.java
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/operations/SampleQueryProviderHandler.java
@@ -3,15 +3,29 @@ package org.eclipse.jnosql.mapping.core.repository.operations;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.jnosql.mapping.ProviderQuery;
+import org.eclipse.jnosql.mapping.metadata.repository.RepositoryAnnotation;
+import org.eclipse.jnosql.mapping.metadata.repository.RepositoryMethod;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.ProviderQueryHandler;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+
+import java.util.Map;
 
 @ProviderQuery("sample-query-provider")
 @ApplicationScoped
 public class SampleQueryProviderHandler implements ProviderQueryHandler {
 
+    @SuppressWarnings("unchecked")
     @Override
     public <T> T execute(RepositoryInvocationContext context) {
-        return null;
+
+        RepositoryMethod method = context.method();
+        Object[] parameters = context.parameters();
+        var sampleQueryProvider = method.annotations().stream()
+                .filter(annotation -> SampleQueryProvider.class.equals(annotation.annotation()))
+                .findFirst().orElseThrow();
+        Map<String, Object> attributes = sampleQueryProvider.attributes();
+        String value = (String) attributes.get("value");
+
+        return (T) (value + " " + parameters[0]);
     }
 }
