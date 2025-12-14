1efddd12b1d9a816465c00f7f71ca0200cc2a57d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-14 19:56:48 +0000

    feat: implement executeFindByQuery and mapper methods in SemistructuredReturnType

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredReturnType.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredReturnType.java
@@ -14,8 +14,73 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.repository;
 
+import jakarta.data.repository.Select;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.jnosql.communication.semistructured.SelectQuery;
+import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.ProjectionMetadata;
+import org.eclipse.jnosql.mapping.metadata.repository.RepositoryMethod;
+import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+import org.eclipse.jnosql.mapping.semistructured.SemiStructuredTemplate;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 class SemistructuredReturnType {
+
+    private EntitiesMetadata entitiesMetadata;
+
+    @SuppressWarnings("unchecked")
+    protected Object executeFindByQuery(RepositoryInvocationContext context, SelectQuery query) {
+
+        var method = context.method();
+        var template = (SemiStructuredTemplate) context.template();
+        var entityMetadata = context.entityMetadata();
+        var typeClass = entityMetadata.type();
+        DynamicReturn<?> dynamicReturn = DynamicReturn.builder()
+                .classSource(typeClass)
+                .methodName(method.name())
+                .returnType(method.returnType().orElseThrow())
+
+                .result(() -> {
+                    Stream<Object> select = template.select(query);
+                    return select.map(mapper(method));
+                })
+                .singleResult(() -> {
+                    Optional<Object> object = template.singleResult(query);
+                    return object.map(mapper(method));
+                })
+                .pagination(DynamicReturn.findPageRequest(args))
+                .streamPagination(streamPagination(query, method))
+                .singleResultPagination(getSingleResult(query, method))
+                .page(getPage(query, method))
+                .build();
+        return dynamicReturn.execute();
+    }
+
+    protected <E> Function<Object, E> mapper(RepositoryInvocationContext context) {
+        return value -> {
+            RepositoryMethod method = context.method();
+            var entityMetadata = context.entityMetadata();
+            var returnType = method.elementType().orElseThrow();
+            Optional<ProjectionMetadata> projection = this.entitiesMetadata.projection(returnType);
+            if (projection.isPresent()) {
+                ProjectionMetadata projectionMetadata = projection.orElseThrow();
+                return projectorConverter().map(value, projectionMetadata);
+            }
+            Select[] annotations = method.getAnnotationsByType(Select.class);
+            if (annotations.length == 1) {
+                String fieldReturn = annotations[0].value();
+                Optional<EntityMetadata> valueEntityMetadata = entitiesMetadata().findByClassName(value.getClass().getName());
+                return (E) valueEntityMetadata
+                        .map(entityMetadata -> value(entityMetadata, fieldReturn, value))
+                        .orElse(value);
+            }
+            return (E) value;
+        };
+    }
 }
