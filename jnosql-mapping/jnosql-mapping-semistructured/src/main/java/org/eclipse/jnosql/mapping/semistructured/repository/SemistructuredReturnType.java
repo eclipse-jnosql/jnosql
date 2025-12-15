d1f279965c15d0cc055a4e4822945cfa1b5ead78
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-15 17:31:47 +0000

    feat: enhance SemistructuredReturnType with pagination support and improved mapper functionality

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredReturnType.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredReturnType.java
@@ -14,12 +14,16 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.repository;
 
-import jakarta.data.repository.Select;
+import jakarta.data.page.Page;
+import jakarta.data.page.PageRequest;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.jnosql.communication.semistructured.SelectQuery;
+import org.eclipse.jnosql.mapping.core.NoSQLPage;
 import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
 import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
 import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
 import org.eclipse.jnosql.mapping.metadata.ProjectionMetadata;
 import org.eclipse.jnosql.mapping.metadata.repository.RepositoryMethod;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
@@ -29,14 +33,26 @@ import org.eclipse.jnosql.mapping.semistructured.SemiStructuredTemplate;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @ApplicationScoped
 class SemistructuredReturnType {
 
-    private EntitiesMetadata entitiesMetadata;
+    private final EntitiesMetadata entitiesMetadata;
 
-    private ProjectorConverter projectorConverter;
+    private final  ProjectorConverter projectorConverter;
+
+    @Inject
+    SemistructuredReturnType(EntitiesMetadata entitiesMetadata, ProjectorConverter projectorConverter) {
+        this.entitiesMetadata = entitiesMetadata;
+        this.projectorConverter = projectorConverter;
+    }
+
+    SemistructuredReturnType() {
+        this.entitiesMetadata = null;
+        this.projectorConverter = null;
+    }
 
     @SuppressWarnings("unchecked")
     protected Object executeFindByQuery(RepositoryInvocationContext context, SelectQuery query) {
@@ -58,14 +74,15 @@ class SemistructuredReturnType {
                     Optional<Object> object = template.singleResult(query);
                     return object.map(mapper(method));
                 })
-                .pagination(DynamicReturn.findPageRequest(args))
-                .streamPagination(streamPagination(query, method))
-                .singleResultPagination(getSingleResult(query, method))
-                .page(getPage(query, method))
+                .pagination(DynamicReturn.findPageRequest(context.parameters()))
+                .streamPagination(streamPagination(query, method, template))
+                .singleResultPagination(getSingleResult(query, method, template))
+                .page(getPage(query, method, template))
                 .build();
         return dynamicReturn.execute();
     }
 
+    @SuppressWarnings("unchecked")
     protected <E> Function<Object, E> mapper(RepositoryInvocationContext context) {
         return value -> {
             RepositoryMethod method = context.method();
@@ -85,5 +102,66 @@ class SemistructuredReturnType {
             }
             return (E) value;
         };
+    }
+
+    protected <T> Function<PageRequest, Stream<T>> streamPagination(SelectQuery query,
+                                                                    RepositoryMethod method,
+                                                                    SemiStructuredTemplate template) {
+        return p -> template.select(query).map(mapper(method));
+    }
+
+    protected <T> Function<PageRequest, Optional<T>> getSingleResult(SelectQuery query,
+                                                                     RepositoryMethod method,
+                                                                 SemiStructuredTemplate template) {
+        return p -> template.singleResult(query).map(mapper(method));
+    }
+
+    protected <T>  Function<PageRequest, Page<T>> getPage(SelectQuery query,
+                                                          RepositoryMethod method,
+                                                          SemiStructuredTemplate template) {
+        return p -> {
+            Stream<T> entities = template.select(query).map(mapper(method));
+            return NoSQLPage.of(entities.toList(), p);
+        };
+    }
+
+
+    @SuppressWarnings("unchecked")
+    protected <E> Function<Object, E> mapper(RepositoryMethod method) {
+        return value -> {
+            var returnType = method.elementType().orElse(method.returnType().orElseThrow());
+            Optional<ProjectionMetadata> projection = this.entitiesMetadata.projection(returnType);
+            if (projection.isPresent()) {
+                ProjectionMetadata projectionMetadata = projection.orElseThrow();
+                return projectorConverter.map(value, projectionMetadata);
+            }
+            var annotations = method.select();
+            if (annotations.size() == 1) {
+                String fieldReturn = annotations.getFirst();
+                Optional<EntityMetadata> valueEntityMetadata = entitiesMetadata.findByClassName(value.getClass().getName());
+                return (E) valueEntityMetadata
+                        .map(entityMetadata -> value(entityMetadata, fieldReturn, value))
+                        .orElse(value);
+            }
+            return (E) value;
+        };
+    }
+
+    private Object value(EntityMetadata entityMetadata, String returnName, Object value) {
+        var names = returnName.split("\\.");
+        Optional<FieldMetadata> fieldMetadata = entityMetadata.fieldMapping(names[0]);
+        if (fieldMetadata.isPresent()) {
+            var field = fieldMetadata.orElseThrow();
+            var convertedField = field.read(value);
+            if (convertedField != null && names.length > 1) {
+                var subField = Stream.of(names).skip(1).collect(Collectors.joining("."));
+                var subEntityMetadata = entitiesMetadata.findByClassName(convertedField.getClass().getName())
+                        .orElseThrow(() -> new IllegalArgumentException("Entity metadata not found for " + convertedField.getClass()));
+                return value(subEntityMetadata, subField, convertedField);
+
+            }
+            return convertedField == null ? value : convertedField;
+        }
+        return value;
     }
 }
