6adab5d33b572c20c9cbde08eca8b3c7651df2a8
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-17 19:54:22 +0000

    feat: enhance DynamicSelectQueryBuilder with improved query condition handling and column/sort updates

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/DynamicSelectQueryBuilder.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/DynamicSelectQueryBuilder.java
@@ -14,6 +14,8 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.repository;
 
+import jakarta.data.Direction;
+import jakarta.data.Sort;
 import org.eclipse.jnosql.communication.semistructured.CommunicationObserverParser;
 import org.eclipse.jnosql.communication.semistructured.CriteriaCondition;
 import org.eclipse.jnosql.communication.semistructured.Element;
@@ -27,56 +29,60 @@ import org.eclipse.jnosql.mapping.semistructured.MappingQuery;
 import org.eclipse.jnosql.mapping.semistructured.query.RestrictionConverter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 enum DynamicSelectQueryBuilder {
 
 INSTANCE;
 
-    SelectQuery updateDynamicQuery(SelectQuery query, RepositoryInvocationContext context,
-                                   CommunicationObserverParser parser
-            , Converters converters) {
+    SelectQuery updateDynamicQuery(SelectQuery query,
+                                   RepositoryInvocationContext context,
+                                   CommunicationObserverParser parser,
+                                   Converters converters) {
+
         var method = context.method();
+        var entityMetadata = context.entityMetadata();
         var specialParameters = SpecialParameters.of(context.parameters(), Function.identity());
+
         var columns = new ArrayList<>(query.columns());
         columns.addAll(method.select());
+
         var sorts = new ArrayList<>(query.sorts());
+        sorts.addAll(method.sorts());
+
         var limit = query.limit();
         var skip = query.skip();
-        sorts.addAll(method.sorts());
-        var condition = query.condition().orElse(null);
-        var conditionInheritance = includeInheritance(context.entityMetadata());
-        if (conditionInheritance != null) {
-            condition = appendCriteriaCondition(condition, conditionInheritance);
-        }
         if (method.first().isPresent()) {
             limit = 0;
             skip = method.first().orElseThrow();
         }
+
         if (specialParameters.limit().isPresent()) {
             var limitParam = specialParameters.limit().orElseThrow();
             limit = limitParam.maxResults();
             skip = limitParam.startAt() - 1;
         }
+
+        var condition = query.condition().orElse(null);
+        var conditionInheritance = includeInheritance(entityMetadata);
+        if (conditionInheritance != null) {
+            condition = appendCriteriaCondition(condition, conditionInheritance);
+        }
+
         if (specialParameters.restriction().isPresent()) {
-            var restriction = specialParameters.restriction().orElseThrow();
-            var restrictionCondition = RestrictionConverter.INSTANCE.parser(restriction, context.entityMetadata(),
+            var restrictionCondition = RestrictionConverter.INSTANCE.parser(specialParameters.restriction().orElseThrow(),
+                    entityMetadata,
                     converters);
             if (restrictionCondition.isPresent()) {
                 condition = appendCriteriaCondition(condition, restrictionCondition.orElseThrow());
             }
         }
-        return new MappingQuery(sorts, limit, skip,
-                condition, query.name(), columns);
-    }
 
-    private static CriteriaCondition appendCriteriaCondition(CriteriaCondition condition, CriteriaCondition newCondition) {
-        if (condition != null) {
-            condition = condition.and(newCondition);
-        } else {
-            condition = newCondition;
-        }
-        return condition;
+        return new MappingQuery(sorts(parser, sorts, entityMetadata),
+                limit, skip,
+                condition, query.name(),
+                columns(parser, columns, entityMetadata));
     }
 
     SelectQuery includeInheritance(SelectQuery query, EntityMetadata metadata) {
@@ -106,5 +112,38 @@ INSTANCE;
     SelectQuery applyInheritance(SelectQuery query, RepositoryInvocationContext context) {
         var entityMetadata = context.entityMetadata();
         return includeInheritance(query, entityMetadata);
+    }
+
+
+
+    private static CriteriaCondition appendCriteriaCondition(CriteriaCondition condition, CriteriaCondition newCondition) {
+        if (condition != null) {
+            condition = condition.and(newCondition);
+        } else {
+            condition = newCondition;
+        }
+        return condition;
+    }
+
+    private static List<String> columns(CommunicationObserverParser parser, ArrayList<String> columns,
+                                        EntityMetadata entityMetadata) {
+        if(columns.isEmpty()) {
+            return columns;
+        }
+        return columns.stream().map(c -> parser.fireSelectField(entityMetadata.name(), c)).toList();
+    }
+
+    private static List<Sort<?>> sorts(CommunicationObserverParser parser,
+                                          List<Sort<?>> sorts,
+                                          EntityMetadata entityMetadata) {
+        if(sorts.isEmpty()) {
+            return sorts;
+        }
+        List<Sort<?>> updateSorts = new ArrayList<>(sorts.size());
+        updateSorts.addAll(sorts.stream().map(sort -> {
+            String attribute = parser.fireSortProperty(entityMetadata.name(), sort.property());
+            return Sort.of(attribute, sort.isAscending() ? Direction.ASC : Direction.DESC, sort.ignoreCase());
+        }).toList());
+        return updateSorts;
     }
 }
