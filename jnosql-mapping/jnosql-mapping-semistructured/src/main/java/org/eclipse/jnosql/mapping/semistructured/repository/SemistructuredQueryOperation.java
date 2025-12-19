ab3c6adf5ea4ca5f9f8c5293257db45c879a6b11
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-19 04:21:25 +0000

    feat: enhance query execution with logging and improved return type handling in SemistructuredQueryOperation

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredQueryOperation.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredQueryOperation.java
@@ -21,21 +21,29 @@ import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
 import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.QueryOperation;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
+import org.eclipse.jnosql.mapping.semistructured.query.AbstractSemiStructuredRepositoryProxy;
+
+import java.util.logging.Logger;
 
 @ApplicationScoped
 class SemistructuredQueryOperation implements QueryOperation {
+
+    private static final Logger LOGGER = Logger.getLogger(SemistructuredQueryOperation.class.getName());
 
     @Override
     public <T> T execute(RepositoryInvocationContext context) {
 
         var entityMetadata = context.entityMetadata();
         var method = context.method();
+        var params = context.parameters();
         Class<?> type = entityMetadata.type();
         var entity = entityMetadata.name();
         var pageRequest = DynamicReturn.findPageRequest(params);
         var queryValue = method.query().orElseThrow();
         var queryType = QueryType.parse(queryValue);
-        var returnType = method.getReturnType();
+        var returnType = method.returnType().orElseThrow();
+        LOGGER.finest("Query: " + queryValue + " with type: " + queryType + " and return type: " + returnType);
+        queryType.checkValidReturn(returnType, queryValue);
         return null;
     }
 }
