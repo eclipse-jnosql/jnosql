be03491f76ad8068b903fc1f3052c90e5cc8bd07
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-21 07:03:45 +0000

    test: add stream method to PersonRepository and corresponding tests for annotation retrieval

--- jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/repository/ReflectionRepositorySupplierTest.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/repository/ReflectionRepositorySupplierTest.java
@@ -611,4 +611,27 @@ class ReflectionRepositorySupplierTest {
         });
     }
 
+    @Test
+    @DisplayName("should not find annotation")
+    void shouldNotFindAnnotation() {
+        RepositoryMetadata metadata = supplier.apply(PersonRepository.class);
+        var method = metadata.find(new NameKey("savePerson")).orElseThrow();
+        Assertions.assertThat(method.find()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should find annotation with default value")
+    void shouldFindAnnotationWithDefaultValue() {
+        RepositoryMetadata metadata = supplier.apply(PersonRepository.class);
+        var method = metadata.find(new NameKey("find")).orElseThrow();
+        Assertions.assertThat(method.find()).isPresent().get().isEqualTo(void.class);
+    }
+
+    @Test
+    @DisplayName("should find annotation with custom value")
+    void shouldFindAnnotationWithCustomValue() {
+        RepositoryMetadata metadata = supplier.apply(PersonRepository.class);
+        var method = metadata.find(new NameKey("stream")).orElseThrow();
+        Assertions.assertThat(method.find()).isPresent().get().isEqualTo(Person.class);
+    }
 }
