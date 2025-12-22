6ba3e704083d7a947ff431a3c3a1b2b8c85ef143
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-22 20:56:32 +0000

    test: add test for error handling when deleting by restriction in ComicBookRepository

--- jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/operations/DeleteOperationRepositoryInvocationHandlerTest.java
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/operations/DeleteOperationRepositoryInvocationHandlerTest.java
@@ -14,6 +14,7 @@
  */
 package org.eclipse.jnosql.mapping.core.repository.operations;
 
+import jakarta.data.metamodel.TextAttribute;
 import jakarta.inject.Inject;
 import jakarta.nosql.Convert;
 import jakarta.nosql.Template;
@@ -113,5 +114,12 @@ class DeleteOperationRepositoryInvocationHandlerTest {
     @Test
     void shouldExecuteEqualsToString() {
         Assertions.assertThat(comicBookRepository.toString()).isNotNull();
+    }
+
+
+    @Test
+    void shouldReturnErrorWhenDeleteByRestriction() {
+        TextAttribute<ComicBook> name = TextAttribute.of(ComicBook.class, "name");
+        Assertions.assertThatThrownBy(() -> comicBookRepository.delete(name.contains("Marvel"))).isInstanceOf(UnsupportedOperationException.class);
     }
 }
