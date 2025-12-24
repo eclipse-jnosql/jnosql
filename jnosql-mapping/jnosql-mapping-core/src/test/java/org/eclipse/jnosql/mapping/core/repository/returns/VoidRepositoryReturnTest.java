68b964e871ab109c88e5e9f1f9ff4c1df13010ae
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-24 14:15:12 +0000

    test: add VoidRepositoryReturnTest to validate Void return type compatibility

---
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/returns/VoidRepositoryReturnTest.java
@@ -0,0 +1,133 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.core.repository.returns;
+
+import jakarta.data.page.Page;
+import jakarta.data.page.PageRequest;
+import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
+import org.eclipse.jnosql.mapping.core.repository.RepositoryReturn;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class VoidRepositoryReturnTest {
+
+    private final RepositoryReturn repositoryReturn = new VoidRepositoryReturn();
+
+    @Mock
+    private Page<Person> page;
+
+    @ParameterizedTest
+    @ValueSource(classes = {void.class, Void.class})
+    void shouldReturnIsCompatible(Class<?> returnType) {
+        assertThat(repositoryReturn.isCompatible(Person.class, returnType)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {List.class, Set.class, Map.class, Iterable.class, Queue.class, Optional.class, Page.class, Person.class})
+    void shouldReturnIsNotCompatible(Class<?> returnType) {
+        assertThat(repositoryReturn.isCompatible(Person.class, returnType)).isFalse();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void shouldReturnNull() {
+        Method method = Person.class.getDeclaredMethods()[0];
+        DynamicReturn<Person> dynamic = DynamicReturn.builder()
+                .classSource(Person.class)
+                .singleResult(Optional::empty)
+                .result(Collections::emptyList)
+                .singleResultPagination(p -> Optional.empty())
+                .streamPagination(p -> Stream.empty())
+                .returnType(Person.class)
+                .methodName(method.getName())
+                .pagination(PageRequest.ofPage(2).size(2))
+                .page(p -> page)
+                .build();
+        Object object = repositoryReturn.convertPageRequest(dynamic);
+        assertNull(object);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void shouldReturnNotNull() {
+        Method method = Person.class.getDeclaredMethods()[0];
+        DynamicReturn<Person> dynamic = DynamicReturn.builder()
+                .singleResult(Optional::empty)
+                .classSource(Person.class)
+                .result(Collections::emptyList)
+                .returnType(Person.class)
+                .methodName(method.getName())
+                .build();
+
+        Object convert = repositoryReturn.convert(dynamic);
+        assertNull(convert);
+    }
+
+    private static class Person implements Comparable<Person> {
+
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public Person() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            return name.compareTo(o.name);
+        }
+    }
+}
