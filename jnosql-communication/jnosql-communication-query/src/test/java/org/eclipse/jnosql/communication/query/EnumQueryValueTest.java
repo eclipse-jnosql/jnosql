997c737aa67f4fd2b4fa62636d62e39b06ecca9b
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-20 17:45:58 +0000

    test: add initial test class for EnumQueryValue

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/EnumQueryValueTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/EnumQueryValueTest.java
@@ -11,8 +11,53 @@
  */
 package org.eclipse.jnosql.communication.query;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("EnumQueryValue")
 class EnumQueryValueTest {
 
+    private enum SampleEnum {
+        FIRST, SECOND
+    }
+
+    @Test
+    @DisplayName("should expose the enum value through the Supplier contract")
+    void shouldReturnEnumValueFromSupplier() {
+        EnumQueryValue queryValue = EnumQueryValue.of(SampleEnum.FIRST);
+
+        assertThat(queryValue.get())
+                .isEqualTo(SampleEnum.FIRST);
+    }
+
+    @Test
+    @DisplayName("should report ENUM as its ValueType")
+    void shouldExposeEnumValueType() {
+        EnumQueryValue queryValue = EnumQueryValue.of(SampleEnum.SECOND);
+
+        assertThat(queryValue.type())
+                .isEqualTo(ValueType.ENUM);
+    }
+
+    @Test
+    @DisplayName("should implement the QueryValue contract")
+    void shouldImplementQueryValueContract() {
+        EnumQueryValue queryValue = EnumQueryValue.of(SampleEnum.FIRST);
+
+        assertThat(queryValue)
+                .isInstanceOf(QueryValue.class);
+    }
+
+    @Test
+    @DisplayName("factory method should create an equivalent instance")
+    void factoryMethodShouldCreateEquivalentInstance() {
+        EnumQueryValue first = EnumQueryValue.of(SampleEnum.FIRST);
+        EnumQueryValue second = new EnumQueryValue(SampleEnum.FIRST);
+
+        assertThat(first)
+                .isEqualTo(second);
+    }
 }
