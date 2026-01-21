aac50493e2905cbdadadfbb337375df4463fc9aa
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-21 20:00:11 +0000

    feat: add ReferenceToken class for semistructured communication

--- jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/ReferenceToken.java
+++ jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/ReferenceToken.java
@@ -10,5 +10,46 @@
  */
 package org.eclipse.jnosql.communication.semistructured;
 
-public class ReferenceToken {
+import java.util.Objects;
+
+/**
+ * Represents a provider-defined reference token used to identify
+ * an addressable element within a database structure.
+ *
+ * <p>The semantic meaning of a {@code ReferenceToken} is entirely
+ * provider-defined. It may refer to a document field, column,
+ * property, or any other addressable element supported by the
+ * underlying database.</p>
+ *
+ * <p>This type is an opaque value object. The specification does
+ * not impose validation rules or structural guarantees.</p>
+ */
+public final class ReferenceToken {
+
+    private final String value;
+
+    public ReferenceToken(String value) {
+        this.value = Objects.requireNonNull(value, "value must not be null");
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ReferenceToken)) return false;
+        return value.equals(((ReferenceToken) o).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
 }
