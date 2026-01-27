5a11cd92bc1a9e882f36a5c14f7082925b2476d5
Otavio Santana <otaviopolianasantana@gmail.com>
2026-01-27 04:47:45 +0000

    test: add unit tests for update functionality in MapperUpdateTest

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/MapperUpdateTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/MapperUpdateTest.java
@@ -14,17 +14,25 @@
  */
 package org.eclipse.jnosql.mapping.semistructured;
 
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
 import org.eclipse.jnosql.communication.semistructured.DatabaseManager;
 import org.eclipse.jnosql.communication.semistructured.UpdateQuery;
 import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
 import org.eclipse.jnosql.mapping.reflection.Reflections;
 import org.eclipse.jnosql.mapping.reflection.spi.ReflectionEntityMetadataExtension;
+import org.eclipse.jnosql.mapping.semistructured.entities.Person;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 @EnableAutoWeld
 @AddPackages(value = {Converters.class, EntityConverter.class})
@@ -47,4 +55,35 @@ public class MapperUpdateTest {
     private DefaultSemiStructuredTemplate template;
 
     private ArgumentCaptor<UpdateQuery> captor;
+
+    @BeforeEach
+    void setUp() {
+        managerMock = Mockito.mock(DatabaseManager.class);
+        EventPersistManager persistManager = Mockito.mock(EventPersistManager.class);
+        Instance<DatabaseManager> instance = Mockito.mock(Instance.class);
+        this.captor = ArgumentCaptor.forClass(UpdateQuery.class);
+        when(instance.get()).thenReturn(managerMock);
+        this.template = new DefaultSemiStructuredTemplate(
+                converter, instance, persistManager, entities, converters);
+    }
+
+
+    @Test
+    void shouldUpdateSingleField() {
+        template.update(Person.class)
+                .set("name").to("Ada")
+                .execute();
+
+        Mockito.verify(template).update(captor.capture());
+        var update = captor.getValue();
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(update.name()).isEqualTo("Person");
+            soft.assertThat(update.set()).hasSize(1);
+            soft.assertThat(update.set().getFirst().name()).isEqualTo("name");
+            soft.assertThat(update.set().getFirst().value()).isEqualTo("Ada");
+        });
+    }
+
+
 }
