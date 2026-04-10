4d2120b6ac7d46419f653c405b614044674e0f7d
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-10 19:36:15 +0100

    docs: add class-level Javadoc to DefaultUpdateQuery for improved documentation

--- jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/DefaultUpdateQuery.java
+++ jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/DefaultUpdateQuery.java
@@ -17,6 +17,16 @@ import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 
+/**
+ * Represents a default implementation of the {@link UpdateQuery} interface for constructing and
+ * handling database update operations. This record encapsulates details such as the target entity name,
+ * the list of field-value pairs to be updated, and the criteria used to filter records for the update.
+ * It also supports converting the update query into a corresponding {@link SelectQuery} to review affected records.
+ *
+ * @param name               the name of the entity targeted by the update query
+ * @param sets               the list of {@link Element} instances representing the fields and their updated values
+ * @param criteriaCondition  the {@link CriteriaCondition} specifying the criteria to filter records for the update
+ */
 public record DefaultUpdateQuery(String name, List<Element> sets, CriteriaCondition criteriaCondition) implements UpdateQuery {
 
     @Override
