95143614b26aa763b07ba521d5eb9a6fd8300c7f
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-15 17:54:26 +0100

    test: add wish list parsing and formatting to Dreams entity

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/entities/autoconverter/Dreams.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/entities/autoconverter/Dreams.java
@@ -1,4 +1,29 @@
 package org.eclipse.jnosql.mapping.semistructured.entities.autoconverter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Dreams {
+
+    private final List<String> wishes = new ArrayList<>();
+
+     public List<String> getWishes() {
+        return Collections.unmodifiableList(wishes);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(",", this.wishes);
+    }
+
+    public static Dreams parse(String value) {
+        Dreams dreams = new Dreams();
+        dreams.wishes.addAll(List.of(value.split(",")));
+        return dreams;
+    }
+
+     public void addWish(String wish) {
+        this.wishes.add(wish);
+    }
 }
