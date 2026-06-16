cbaf3d225850d7cb8b6204a36b8f57c0e1e57940
Otavio Santana <otaviopolianasantana@gmail.com>
2026-06-16 19:36:10 +0100

    refactor: enforce null and format validation in Email record constructor

--- jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/entities/Email.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/entities/Email.java
@@ -1,9 +1,16 @@
 package org.eclipse.jnosql.mapping.reflection.entities;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public record Email(String value) implements Supplier<String> {
 
+    public Email{
+        Objects.requireNonNull(value, "Email value cannot be null");
+        if(!value.contains("@")) {
+            throw new IllegalArgumentException("Invalid email format: " + value);
+        }
+    }
     @Override
     public String get() {
         return value;
