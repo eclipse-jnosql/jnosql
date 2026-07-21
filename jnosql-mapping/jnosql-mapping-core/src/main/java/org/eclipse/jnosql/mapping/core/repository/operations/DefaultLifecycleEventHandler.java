8478a6a75a55ff157d04dfa31345b84e7b33e22a
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-21 17:49:03 +0100

    feat: make DefaultLifecycleEventHandler an ApplicationScoped LifecycleEventHandler bean

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/core/repository/operations/DefaultLifecycleEventHandler.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/core/repository/operations/DefaultLifecycleEventHandler.java
@@ -14,10 +14,89 @@
  */
 package org.eclipse.jnosql.mapping.core.repository.operations;
 
+import jakarta.data.event.PostDeleteEvent;
+import jakarta.data.event.PostInsertEvent;
+import jakarta.data.event.PostUpdateEvent;
+import jakarta.data.event.PostUpsertEvent;
+import jakarta.data.event.PreDeleteEvent;
+import jakarta.data.event.PreInsertEvent;
+import jakarta.data.event.PreUpdateEvent;
+import jakarta.data.event.PreUpsertEvent;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Event;
+import jakarta.inject.Inject;
+
 
 @ApplicationScoped
 class DefaultLifecycleEventHandler implements LifecycleEventHandler {
 
+    private final Event<PreDeleteEvent<?>> preDeleteEvent;
+    private final Event<PreInsertEvent<?>> preInsertEvent;
+    private final Event<PreUpdateEvent<?>> preUpdateEvent;
+    private final Event<PreUpsertEvent<?>> preUpsertEvent;
+    private final Event<PostDeleteEvent<?>> postDeleteEvent;
+    private final Event<PostInsertEvent<?>> postInsertEvent;
+    private final Event<PostUpdateEvent<?>> postUpdateEvent;
+    private final Event<PostUpsertEvent<?>> postUpsertEvent;
+
+    @Inject
+    DefaultLifecycleEventHandler(
+            Event<PreDeleteEvent<?>> preDeleteEvent,
+            Event<PreInsertEvent<?>> preInsertEvent,
+            Event<PreUpdateEvent<?>> preUpdateEvent,
+            Event<PreUpsertEvent<?>> preUpsertEvent,
+            Event<PostDeleteEvent<?>> postDeleteEvent,
+            Event<PostInsertEvent<?>> postInsertEvent,
+            Event<PostUpdateEvent<?>> postUpdateEvent,
+            Event<PostUpsertEvent<?>> postUpsertEvent) {
+        this.preDeleteEvent = preDeleteEvent;
+        this.preInsertEvent = preInsertEvent;
+        this.preUpdateEvent = preUpdateEvent;
+        this.preUpsertEvent = preUpsertEvent;
+        this.postDeleteEvent = postDeleteEvent;
+        this.postInsertEvent = postInsertEvent;
+        this.postUpdateEvent = postUpdateEvent;
+        this.postUpsertEvent = postUpsertEvent;
+    }
+
+    @Override
+    public <T> void preDelete(T entity) {
+        preDeleteEvent.fire(new PreDeleteEvent<>(entity));
+    }
+
+    @Override
+    public <T> void preInsert(T entity) {
+        preInsertEvent.fire(new PreInsertEvent<>(entity));
+    }
+
+    @Override
+    public <T> void preUpdate(T entity) {
+        preUpdateEvent.fire(new PreUpdateEvent<>(entity));
+    }
+
+    @Override
+    public <T> void preUpsert(T entity) {
+        preUpsertEvent.fire(new PreUpsertEvent<>(entity));
+    }
+
+    @Override
+    public <T> void postDelete(T entity) {
+        postDeleteEvent.fire(new PostDeleteEvent<>(entity));
+    }
+
+    @Override
+    public <T> void postInsert(T entity) {
+        postInsertEvent.fire(new PostInsertEvent<>(entity));
+    }
+
+    @Override
+    public <T> void postUpdate(T entity) {
+        postUpdateEvent.fire(new PostUpdateEvent<>(entity));
+    }
+
+    @Override
+    public <T> void postUpsert(T entity) {
+        postUpsertEvent.fire(new PostUpsertEvent<>(entity));
+    }
 
 }
