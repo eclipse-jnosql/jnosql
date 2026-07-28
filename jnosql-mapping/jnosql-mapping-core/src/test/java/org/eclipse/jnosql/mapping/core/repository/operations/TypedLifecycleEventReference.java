02a16a6d3ca7161d54ae2c400118e43ae613fb77
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-28 19:16:46 +0100

    refactor: add typed lifecycle event reference for pre-insert events

---
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/operations/TypedLifecycleEventReference.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.core.repository.operations;
+
+import jakarta.data.event.PreInsertEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Event;
+import jakarta.enterprise.util.TypeLiteral;
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.mapping.core.entities.Book;
+
+import java.util.function.Function;
+
+@ApplicationScoped
+public class TypedLifecycleEventReference {
+
+    @Inject
+    private Event<Object> events;
+
+    void preInsert(Object book) {
+        events.select(BookLifecycleEventTypes.PRE_INSERT)
+                .fire(BookLifecycleEventTypes.PRE_INSERT_FUNCTION.apply(book));
+    }
+
+    final class BookLifecycleEventTypes {
+
+        static final TypeLiteral<PreInsertEvent<Book>> PRE_INSERT =
+                new TypeLiteral<>() {};
+
+        static final Function<Object, PreInsertEvent<Book>> PRE_INSERT_FUNCTION =
+                entity -> new PreInsertEvent<>((Book) entity);
+
+        // Remaining lifecycle types
+    }
+}
