f25757dbe1d9f7605fa8ed200ef315a9cdf0b936
Otavio Santana <otaviopolianasantana@gmail.com>
2026-07-28 19:22:11 +0100

    refactor: move LifecycleEventHandler to mapping.repository package

--- jnosql-mapping/jnosql-mapping-api-core/src/main/java/org/eclipse/jnosql/mapping/repository/LifecycleEventHandler.java
+++ jnosql-mapping/jnosql-mapping-api-core/src/main/java/org/eclipse/jnosql/mapping/repository/LifecycleEventHandler.java
@@ -12,7 +12,7 @@
  *
  *   Otavio Santana
  */
-package org.eclipse.jnosql.mapping.core.repository.operations;
+package org.eclipse.jnosql.mapping.repository;
 
 /**
  * Handles Jakarta Data entity lifecycle events triggered by Eclipse JNoSQL
