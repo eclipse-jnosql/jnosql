848362445a4d17e5bf23a7d81e9d5ab8fbc06016
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-02 07:12:33 +0100

    test: implement VinylRecordLifecycleObserver to capture lifecycle events

--- jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/events/VinylRecordLifecycleObserver.java
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/repository/events/VinylRecordLifecycleObserver.java
@@ -14,5 +14,84 @@
  */
 package org.eclipse.jnosql.mapping.core.repository.events;
 
-public class VinylRecordLifecycleObserver {
+import jakarta.data.event.PostDeleteEvent;
+import jakarta.data.event.PostInsertEvent;
+import jakarta.data.event.PostUpdateEvent;
+import jakarta.data.event.PostUpsertEvent;
+import jakarta.data.event.PreDeleteEvent;
+import jakarta.data.event.PreInsertEvent;
+import jakarta.data.event.PreUpdateEvent;
+import jakarta.data.event.PreUpsertEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+class VinylRecordLifecycleObserver {
+
+    private final List<ObservedEvent> events = new ArrayList<>();
+
+    void onPreInsert(@Observes PreInsertEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "pre-insert",
+                event.entity()));
+    }
+
+    void onPostInsert(@Observes PostInsertEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "post-insert",
+                event.entity()));
+    }
+
+    void onPreUpdate(
+            @Observes PreUpdateEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "pre-update",
+                event.entity()));
+    }
+
+    void onPostUpdate(
+            @Observes PostUpdateEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "post-update",
+                event.entity()));
+    }
+
+    void onPreUpsert(
+            @Observes PreUpsertEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "pre-upsert",
+                event.entity()));
+    }
+
+    void onPostUpsert(
+            @Observes PostUpsertEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "post-upsert",
+                event.entity()));
+    }
+
+    void onPreDelete(
+            @Observes PreDeleteEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "pre-delete",
+                event.entity()));
+    }
+
+    void onPostDelete(
+            @Observes PostDeleteEvent<VinylRecord> event) {
+        events.add(new ObservedEvent(
+                "post-delete",
+                event.entity()));
+    }
+
+    List<ObservedEvent> events() {
+        return List.copyOf(events);
+    }
+
+    void reset() {
+        events.clear();
+    }
 }
