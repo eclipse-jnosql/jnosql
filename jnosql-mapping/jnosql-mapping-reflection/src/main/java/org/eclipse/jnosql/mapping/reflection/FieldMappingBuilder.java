aa77542cc4026ae5264ed24d4a4aaf7108db46a3
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-06 13:12:00 +0100

    chore: add Javadoc for EnumQueryValue record and of factory method

--- jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/FieldMappingBuilder.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/main/java/org/eclipse/jnosql/mapping/reflection/FieldMappingBuilder.java
@@ -48,78 +48,169 @@ class FieldMappingBuilder {
     private Class<?> elementType;
 
 
-    public FieldMappingBuilder type(MappingType type) {
+    /**
+     * Sets the mapping type.
+     *
+     * @param type the mapping type
+     * @return this builder
+     */
+    FieldMappingBuilder type(MappingType type) {
         this.type = type;
         return this;
     }
 
-    public FieldMappingBuilder field(Field field) {
+    /**
+     * Sets the Java field.
+     *
+     * @param field the Java field
+     * @return this builder
+     */
+    FieldMappingBuilder field(Field field) {
         this.field = field;
         return this;
     }
 
-    public FieldMappingBuilder name(String name) {
+    /**
+     * Sets the mapped field name.
+     *
+     * @param name the mapped field name
+     * @return this builder
+     */
+    FieldMappingBuilder name(String name) {
         this.name = name;
         return this;
     }
 
-    public FieldMappingBuilder typeSupplier(TypeSupplier<?> typeSupplier) {
+    /**
+     * Sets the field type supplier.
+     *
+     * @param typeSupplier the type supplier
+     * @return this builder
+     */
+    FieldMappingBuilder typeSupplier(TypeSupplier<?> typeSupplier) {
         this.typeSupplier = typeSupplier;
         return this;
     }
 
-    public FieldMappingBuilder entityName(String entityName) {
+    /**
+     * Sets the embedded entity name.
+     *
+     * @param entityName the entity name
+     * @return this builder
+     */
+    FieldMappingBuilder entityName(String entityName) {
         this.entityName = entityName;
         return this;
     }
 
-    public FieldMappingBuilder converter(Class<? extends AttributeConverter<?, ?>> converter) {
+    /**
+     * Sets the attribute converter type.
+     *
+     * @param converter the converter type
+     * @return this builder
+     */
+    FieldMappingBuilder converter(Class<? extends AttributeConverter<?, ?>> converter) {
         this.converter = converter;
         return this;
     }
 
-    public FieldMappingBuilder id(boolean id) {
+    /**
+     * Sets whether the field is an identifier.
+     *
+     * @param id whether the field is an identifier
+     * @return this builder
+     */
+    FieldMappingBuilder id(boolean id) {
         this.id = id;
         return this;
     }
 
-    public FieldMappingBuilder writer(FieldWriter writer) {
+    /**
+     * Sets the field writer.
+     *
+     * @param writer the field writer
+     * @return this builder
+     */
+    FieldMappingBuilder writer(FieldWriter writer) {
         this.writer = writer;
         return this;
     }
 
-    public FieldMappingBuilder udt(String udt) {
+    /**
+     * Sets the user-defined type name.
+     *
+     * @param udt the user-defined type name
+     * @return this builder
+     */
+    FieldMappingBuilder udt(String udt) {
         this.udt = udt;
         return this;
     }
 
-    public FieldMappingBuilder reader(FieldReader reader) {
+    /**
+     * Sets the field reader.
+     *
+     * @param reader the field reader
+     * @return this builder
+     */
+    FieldMappingBuilder reader(FieldReader reader) {
         this.reader = reader;
         return this;
     }
 
-    public FieldMappingBuilder elementType(Class<?> elementType) {
+    /**
+     * Sets the array element type.
+     *
+     * @param elementType the element type
+     * @return this builder
+     */
+    FieldMappingBuilder elementType(Class<?> elementType) {
         this.elementType = elementType;
         return this;
     }
 
-    public DefaultFieldMetadata buildDefault() {
+    /**
+     * Builds default field metadata.
+     *
+     * @return the field metadata
+     */
+    DefaultFieldMetadata buildDefault() {
         return new DefaultFieldMetadata(type, field, name, converter, id, reader, writer, udt);
     }
 
-    public CollectionFieldMetadata buildCollection() {
+    /**
+     * Builds collection field metadata.
+     *
+     * @return the collection field metadata
+     */
+    CollectionFieldMetadata buildCollection() {
         return new DefaultCollectionFieldMetadata(type, field, name, typeSupplier, converter, reader, writer, udt);
     }
 
-    public MapFieldMetadata buildMap() {
+    /**
+     * Builds map field metadata.
+     *
+     * @return the map field metadata
+     */
+    MapFieldMetadata buildMap() {
         return new DefaultMapFieldMetadata(type, field, name, typeSupplier, converter, reader, writer, udt);
     }
 
-    public EmbeddedFieldMetadata buildEmbedded() {
+    /**
+     * Builds embedded field metadata.
+     *
+     * @return the embedded field metadata
+     */
+    EmbeddedFieldMetadata buildEmbedded() {
         return new EmbeddedFieldMetadata(type, field, name, entityName, reader, writer, udt);
     }
 
-    public ArrayFieldMetadata buildArray() {
+    /**
+     * Builds array field metadata.
+     *
+     * @return the array field metadata
+     */
+    ArrayFieldMetadata buildArray() {
         return new DefaultArrayFieldMetadata(type, field, name, elementType, converter, reader, writer, udt);
     }
 
