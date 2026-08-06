0eafe2fb450a54c867f9cf934c78109129029554
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-06 13:06:11 +0100

    chore: add Javadoc for CustomRepositoryGraphBean constructor

--- jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/configuration/DocumentManagerSupplier.java
+++ jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/configuration/DocumentManagerSupplier.java
@@ -62,6 +62,11 @@ class DocumentManagerSupplier implements Supplier<DatabaseManager> {
         return manager;
     }
 
+    /**
+     * Closes a disposed document database manager.
+     *
+     * @param manager the document database manager
+     */
     public void close(@Disposes @Database(DatabaseType.DOCUMENT) DatabaseManager manager) {
         LOGGER.log(Level.FINEST, "Closing DocumentManager resource, database name: " + manager.name());
         manager.close();
