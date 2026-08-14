5e0206e2c24e1d9e31cacac3c530da09235d31ab
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 17:24:21 +0100

    test: improve DefaultDocumentTemplateProducerTest structure and assertion style

--- jnosql-mapping/jnosql-mapping-api-core/src/test/java/org/eclipse/jnosql/mapping/EntityPostPersistTest.java
+++ jnosql-mapping/jnosql-mapping-api-core/src/test/java/org/eclipse/jnosql/mapping/EntityPostPersistTest.java
@@ -14,54 +14,81 @@
  */
 package org.eclipse.jnosql.mapping;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class EntityPostPersistTest {
 
-    @Test
-    void shouldGet() {
-        Object value = new Object();
-        EntityPostPersist entity = new EntityPostPersist(value);
-        assertEquals(value, entity.get());
+    @Nested
+    @DisplayName("When the event is created")
+    class WhenTheEventIsCreated {
+
+        @Test
+        @DisplayName("Should expose the wrapped value")
+        void shouldExposeTheWrappedValue() {
+            Object value = new Object();
+            EntityPostPersist entity = new EntityPostPersist(value);
+
+            assertThat(entity.get()).isSameAs(value);
+        }
+
+        @Test
+        @DisplayName("Should create an event from a non-null value")
+        void shouldCreateAnEventFromANonNullValue() {
+            Object value = new Object();
+            EntityPostPersist entity = EntityPostPersist.of(value);
+
+            assertThat(entity.get()).isSameAs(value);
+        }
+
+        @Test
+        @DisplayName("Should reject a null value")
+        void shouldRejectANullValue() {
+            assertThatNullPointerException().isThrownBy(() -> EntityPostPersist.of(null));
+        }
     }
 
-    @Test
-    void shouldEqualsAndHashCode() {
-        Object value1 = new Object();
-        Object value2 = new Object();
+    @Nested
+    @DisplayName("When the event is compared")
+    class WhenTheEventIsCompared {
 
-        EntityPostPersist entity1 = new EntityPostPersist(value1);
-        EntityPostPersist entity2 = new EntityPostPersist(value1);
-        EntityPostPersist entity3 = new EntityPostPersist(value2);
+        @Test
+        @DisplayName("Should use the wrapped value for equality")
+        void shouldUseTheWrappedValueForEquality() {
+            Object value1 = new Object();
+            Object value2 = new Object();
 
-        assertEquals(entity1, entity1);
-        assertEquals(entity1, entity2);
-        assertEquals(entity2, entity1);
-        assertEquals(entity1, entity2);
-        assertNotEquals(null, entity1);
+            EntityPostPersist entity1 = new EntityPostPersist(value1);
+            EntityPostPersist entity2 = new EntityPostPersist(value1);
+            EntityPostPersist entity3 = new EntityPostPersist(value2);
+
+            assertSoftly(softly -> {
+                softly.assertThat(entity1).isEqualTo(entity1);
+                softly.assertThat(entity1).isEqualTo(entity2);
+                softly.assertThat(entity2).isEqualTo(entity1);
+                softly.assertThat(entity1).isNotEqualTo(entity3);
+                softly.assertThat(entity1).isNotEqualTo(null);
+            });
+        }
     }
 
-    @Test
-    void shouldToString() {
-        Object value = new Object();
-        EntityPostPersist entity = new EntityPostPersist(value);
-        String expected = "EntityPostPersist{value=" + value + "}";
-        assertEquals(expected, entity.toString());
-    }
+    @Nested
+    @DisplayName("When the event is represented as text")
+    class WhenTheEventIsRepresentedAsText {
 
-    @Test
-    void shouldOf() {
-        Object value = new Object();
-        EntityPostPersist entity = EntityPostPersist.of(value);
-        assertEquals(value, entity.get());
-    }
+        @Test
+        @DisplayName("Should include the wrapped value")
+        void shouldIncludeTheWrappedValue() {
+            Object value = new Object();
+            EntityPostPersist entity = new EntityPostPersist(value);
+            String expected = "EntityPostPersist{value=" + value + "}";
 
-    @Test
-    void shouldOfWithNullValue() {
-        assertThrows(NullPointerException.class, () -> EntityPostPersist.of(null));
+            assertThat(entity).hasToString(expected);
+        }
     }
 }
