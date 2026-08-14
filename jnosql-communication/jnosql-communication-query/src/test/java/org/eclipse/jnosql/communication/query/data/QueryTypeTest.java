c95675ceef5ffd8dedec2a5bb462db36c56317b9
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 18:13:09 +0100

    test: organize structure test

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/QueryTypeTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/QueryTypeTest.java
@@ -12,6 +12,8 @@ package org.eclipse.jnosql.communication.query.data;
 
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -21,8 +23,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class QueryTypeTest {
 
+    @Nested
+    @DisplayName("When the query type is used")
+    class WhenTheQueryType {
+    }
+
 
     @Test
+    @DisplayName("Should Parse Select Query")
     void shouldParseSelectQuery() {
         String query = "SELECT * FROM table";
         QueryType result = QueryType.parse(query);
@@ -30,6 +38,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Parse Delete Query")
     void shouldParseDeleteQuery() {
         String query = "DELETE FROM table WHERE id = 1";
         QueryType result = QueryType.parse(query);
@@ -37,6 +46,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Parse Update Query")
     void shouldParseUpdateQuery() {
         String query = "UPDATE table SET name = 'newName' WHERE id = 1";
         QueryType result = QueryType.parse(query);
@@ -44,6 +54,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Default To Select For Unknown Query")
     void shouldDefaultToSelectForUnknownQuery() {
         String query = "INSERT INTO table (id, name) VALUES (1, 'name')";
         QueryType result = QueryType.parse(query);
@@ -51,6 +62,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Default To Select For Short Query")
     void shouldDefaultToSelectForShortQuery() {
         String query = "DELE";
         QueryType result = QueryType.parse(query);
@@ -58,6 +70,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Default To Select For Empty Query")
     void shouldDefaultToSelectForEmptyQuery() {
         String query = "";
         QueryType result = QueryType.parse(query);
@@ -65,6 +78,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Throw Null Pointer Exception For Null Query")
     void shouldThrowNullPointerExceptionForNullQuery() {
         String query = null;
         assertThatThrownBy(() -> QueryType.parse(query))
@@ -72,6 +86,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Return Is Not Select")
     void shouldReturnIsNotSelect() {
         Assertions.assertThat(QueryType.SELECT.isNotSelect()).isFalse();
         Assertions.assertThat(QueryType.DELETE.isNotSelect()).isTrue();
@@ -79,6 +94,7 @@ class QueryTypeTest {
     }
 
     @Test
+    @DisplayName("Should Check Valid Return")
     void shouldCheckValidReturn() {
         QueryType.SELECT.checkValidReturn(String.class, "SELECT * FROM table");
         QueryType.DELETE.checkValidReturn(Void.class, "DELETE FROM table WHERE id = 1");
@@ -90,12 +106,14 @@ class QueryTypeTest {
     }
 
     @ParameterizedTest
+    @DisplayName("Should Is Void")
     @ValueSource(classes = {Void.class, void.class})
     void shouldIsVoid(Class<?> type) {
         Assertions.assertThat(QueryType.SELECT.isVoid(type)).isTrue();
     }
 
     @ParameterizedTest
+    @DisplayName("Should Not Is Void")
     @ValueSource(classes = {String.class, Integer.class})
     void shouldNotIsVoid(Class<?> type) {
         Assertions.assertThat(QueryType.SELECT.isVoid(type)).isFalse();
@@ -103,6 +121,7 @@ class QueryTypeTest {
 
 
     @Test
+    @DisplayName("Should Check Value Return")
     void shouldCheckValueReturn() {
         QueryType type = QueryType.UPDATE;
         Assertions.assertThatThrownBy(() -> type.checkValidReturn(String.class, "UPDATE table SET name = 'newName' WHERE id = 1"))
