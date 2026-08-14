bf213b0c7f56ce6e8d4393c6a5e992e9c1ee2a01
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 18:15:22 +0100

    test: organize structure test

--- jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/query/AbstractRepositoryProxyTest.java
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/core/query/AbstractRepositoryProxyTest.java
@@ -14,89 +14,103 @@
  */
 package org.eclipse.jnosql.mapping.core.query;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+
 import jakarta.data.restrict.Restriction;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AbstractRepositoryProxyTest {
 
     private final TestRepositoryProxy proxy = new TestRepositoryProxy();
 
+    @DisplayName("Should invoke execute find by query")
     @Test
     void shouldInvokeExecuteFindByQuery() throws Throwable {
         Method method = TestRepository.class.getMethod("findEntityById", UUID.class);
         Object result = proxy.invoke(proxy, method, new Object[]{UUID.randomUUID()});
 
-        assertEquals("executeFindByQuery", result);
+        assertThat(result).isEqualTo("executeFindByQuery");
     }
 
+    @DisplayName("Should invoke execute delete by id")
     @Test
     void shouldInvokeExecuteDeleteById() throws Throwable {
         Method method = TestRepository.class.getMethod("deleteById");
         Object result = proxy.invoke(proxy, method, new Object[]{});
 
-        assertEquals("executeDeleteByAll", result);
+        assertThat(result).isEqualTo("executeDeleteByAll");
     }
 
+    @DisplayName("Should invoke execute count by id")
     @Test
     void shouldInvokeExecuteCountById() throws Throwable {
         Method method = TestRepository.class.getMethod("countBy");
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeCountByQuery", result);
+        assertThat(result).isEqualTo("executeCountByQuery");
     }
 
+    @DisplayName("Should invoke execute exist by id")
     @Test
     void shouldInvokeExecuteExistById() throws Throwable {
         Method method = TestRepository.class.getMethod("existsBy");
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeExistByQuery", result);
+        assertThat(result).isEqualTo("executeExistByQuery");
     }
 
+    @DisplayName("Should invoke execute find all")
     @Test
     void shouldInvokeExecuteFindAll() throws Throwable {
         Method method = TestRepository.class.getMethod("findAll");
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeFindAll", result);
+        assertThat(result).isEqualTo("executeFindAll");
     }
 
+    @DisplayName("Should invoke execute query")
     @Test
     void shouldInvokeExecuteQuery() throws Throwable {
         Method method = TestRepository.class.getMethod("query", int.class);
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeQuery", result);
+        assertThat(result).isEqualTo("executeQuery");
     }
 
+    @DisplayName("Should invoke execute cursor")
     @Test
     void shouldInvokeExecuteCursor() throws Throwable {
         Method method = TestRepository.class.getMethod("cursor");
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeCursorPagination", result);
+        assertThat(result).isEqualTo("executeCursorPagination");
     }
 
+    @DisplayName("Should invoke execute find")
     @Test
     void shouldInvokeExecuteFind() throws Throwable {
         Method method = TestRepository.class.getMethod("find");
         Object result = proxy.invoke(proxy, method, new Object[]{});
-        assertEquals("executeParameterBased", result);
+        assertThat(result).isEqualTo("executeParameterBased");
     }
 
+    @DisplayName("Should invoke throws mapping exception")
     @Test
     void shouldInvokeThrowsMappingException() throws Throwable {
         Method method = TestRepository.class.getMethod("customMethod");
 
-        assertThrows(UnsupportedOperationException.class, () -> proxy.invoke(proxy, method, new Object[]{}));
+        assertThatExceptionOfType(UnsupportedOperationException.class).isThrownBy(() -> proxy.invoke(proxy, method, new Object[]{}));
     }
 
+    @DisplayName("Should execute delete restriction")
     @Test
     void shouldExecuteDeleteRestriction() throws Throwable {
         Method method = TestRepository.class.getMethod("delete", Restriction.class);
         Object result = proxy.invoke(proxy, method, new Object[]{(Restriction<String>) () -> null});
-        assertEquals("executeDeleteRestriction", result);
+        assertThat(result).isEqualTo("executeDeleteRestriction");
     }
 
 }
