11d124d20e3d4b4fe2b40a575c5f7cdcec46805e
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 19:32:41 +0100

    test: organize structure test

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredQueryOperationTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredQueryOperationTest.java
@@ -16,13 +16,23 @@ package org.eclipse.jnosql.mapping.semistructured.repository;
 
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SemistructuredQueryOperationTest {
 
+    @DisplayName("Should create instance with default constructor")
     @Test
     void shouldCreateInstanceWithDefaultConstructor() {
         SemistructuredQueryOperation semistructuredQueryOperation = new SemistructuredQueryOperation();
         Assertions.assertThat(semistructuredQueryOperation).isNotNull();
+    }
+
+    @Nested
+    @DisplayName("When the semistructured query operation is tested")
+    class WhenTheSemistructuredQueryOperationIsTested {
     }
 }
