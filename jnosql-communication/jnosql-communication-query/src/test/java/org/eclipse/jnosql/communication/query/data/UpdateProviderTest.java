94d8153e86f957454ba31f09ab77faa31f2333eb
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 18:14:09 +0100

    test: organize structure test

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/UpdateProviderTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/UpdateProviderTest.java
@@ -12,12 +12,20 @@
 package org.eclipse.jnosql.communication.query.data;
 
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class UpdateProviderTest {
 
+    @Nested
+    @DisplayName("When the update provider is used")
+    class WhenTheUpdateProvider {
+    }
+
 
     @Test
+    @DisplayName("Should Generate Update Query")
     void shouldGenerateUpdateQuery() {
         UpdateProvider provider = UpdateProvider.INSTANCE;
 
