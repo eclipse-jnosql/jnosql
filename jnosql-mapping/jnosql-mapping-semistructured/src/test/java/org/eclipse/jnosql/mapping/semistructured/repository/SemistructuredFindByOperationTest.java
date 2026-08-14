5a0aac56267c3c45560c4384a180265c5d4dfc7c
Otavio Santana <otaviopolianasantana@gmail.com>
2026-08-14 19:32:36 +0100

    test: organize structure test

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredFindByOperationTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/repository/SemistructuredFindByOperationTest.java
@@ -14,15 +14,23 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SemistructuredFindByOperationTest {
 
+    @DisplayName("Should instantiate")
     @Test
     void shouldInstantiate() {
         SemistructuredFindByOperation operation = new SemistructuredFindByOperation();
-        assertNotNull(operation);
+        assertThat(operation).isNotNull();
+    }
+
+    @Nested
+    @DisplayName("When the semistructured find by operation is tested")
+    class WhenTheSemistructuredFindByOperationIsTested {
     }
 }
