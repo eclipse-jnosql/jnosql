c377f90631971d074caa8fb2bbc04ce5dc419cec
Otavio Santana <otaviopolianasantana@gmail.com>
2026-09-09 08:54:40 +0100

    test: fix typo in People mock variable name in TimeSeriesCustomExtensionTest

--- jnosql-mapping/jnosql-mapping-timeseries/src/test/java/org/eclipse/jnosql/mapping/timeseries/spi/TimeSeriesCustomExtensionTest.java
+++ jnosql-mapping/jnosql-mapping-timeseries/src/test/java/org/eclipse/jnosql/mapping/timeseries/spi/TimeSeriesCustomExtensionTest.java
@@ -49,7 +49,7 @@ class TimeSeriesCustomExtensionTest {
 
     @Inject
     @Database(value = DatabaseType.TIME_SERIES, provider = "timeseriesRepositoryMock")
-    private People pepoleMock;
+    private People peopleMock;
 
     @Inject
     private People repository;
@@ -78,11 +78,11 @@ class TimeSeriesCustomExtensionTest {
         void shouldInjectProviderCustomRepository() {
 
             // When
-            Person person = pepoleMock.insert(Person.builder().build());
+            Person person = peopleMock.insert(Person.builder().build());
 
             // Then
             assertSoftly(softly -> {
-                softly.assertThat(pepoleMock).isNotNull();
+                softly.assertThat(peopleMock).isNotNull();
                 softly.assertThat(person).isNotNull();
                 softly.assertThat(person.getName()).isEqualTo("timeseriesRepositoryMock");
             });
